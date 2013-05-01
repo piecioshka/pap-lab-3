f8c548a9f6c15ae0dca0b34a337a41da75456281
piecioshka <piecioshka@gmail.com>
2013-05-01 17:16:51 +0200

    create client and server and library utils

--- source/zad1/client.c
+++ source/zad1/client.c
@@ -1,36 +1,38 @@
 /* client - lab3-4 - zadanie 1 */
 
-#include <sys/socket.h>
+#include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
 
 #include "../../library/utils.h"
 
-int create_socket() {
-    int socket_id;
-
-    /* run socket function */
-    socket_id = socket(PF_INET, SOCK_STREAM, 0);
-
-    /* if error occur - send message to user */
-    if (socket_id == -1) {
-        fprintf(stderr, "ERROR: unable to create socket\n");
-        exit(0);
-    }
-
-    printf("create socket: %d\n", socket_id);
-
-    return socket_id;
-}
+#define PORT 13
+#define DESC_IP "127.0.0.1"
 
 int main () {
     /* test_console_lib(); */
-    int i = 10;
+    int sock_id;
+    struct sockaddr_in address;
 
-    printf("create client instance\n");
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = inet_addr(DESC_IP);
+    address.sin_port = htons(PORT);
+    memset( &( address.sin_zero ), '\0', 8 );
 
-    while (--i) {
-        create_socket();
-    }
+    printf("=== create client instance ===\n\n");
+
+    /* create socket to connection */
+    sock_id = create_socket_tcp();
+
+    /* use create socket to connect address */
+    create_connection(sock_id, address);
+
+    /* close connection */
+    close_connection(sock_id);
 
     return 0;
 }
--- source/zad1/server.c
+++ source/zad1/server.c
@@ -1,13 +1,41 @@
 /* server - lab3-4 - zadanie 1 */
 
+#include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
 
 #include "../../library/utils.h"
 
-int main () {
+#define PORT 13
+#define MAX_QUEUE 128
 
-    printf("create server!\n");
-    printf("listening....\n");
+int main () {
+    /* test_console_lib(); */
+    int sock_id;
+    struct sockaddr_in address;
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = htonl(INADDR_ANY);
+    address.sin_port = htons(PORT);
+    memset( &( address.sin_zero ), '\0', 8 );
+
+    printf("=== create server ===\n\n");
+
+    /* create socket */
+    sock_id = create_socket_tcp();
+
+    /* bind server to defined port */
+    bind_port(sock_id, address);
+
+    /* server waiting for client */
+    listen_for_client(sock_id, MAX_QUEUE);
+
+    /* handle client */
+    handle_incoming_client(sock_id);
 
     return 0;
 }
